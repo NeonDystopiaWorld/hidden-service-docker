c85dc9f151e560dc31a9216f2f26ab6e5762e639
XaetaCore <xaetacore@pm.me>
2025-11-07 16:36:35 +0100

    Fixed permissions

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 if [ -e "/data/hostname" ] && [ -e "/data/hs_ed25519_public_key" ] && [ -e "/data/hs_ed25519_secret_key" ]; then 
+  chown -R debian-tor:debian-tor /data 
   cp -r /data/* /var/lib/tor/hidden_service/
   chown -R debian-tor:debian-tor /var/lib/tor/hidden_service/
   chmod 700 /var/lib/tor/hidden_service/
