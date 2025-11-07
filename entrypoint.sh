e5344e99a9f7ceb387aab11b55ca9f31ed58c5e2
XaetaCore <xaetacore@pm.me>
2025-11-07 16:33:54 +0100

    Copying data before chowning

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 if [ -e "/data/hostname" ] && [ -e "/data/hs_ed25519_public_key" ] && [ -e "/data/hs_ed25519_secret_key" ]; then 
-  chown -R debian-tor:debian-tor /var/lib/tor/hidden_service/
   cp -r /data/* /var/lib/tor/hidden_service/
+  chown -R debian-tor:debian-tor /var/lib/tor/hidden_service/
   chmod 700 /var/lib/tor/hidden_service/
   chmod 600 /var/lib/tor/hidden_service/hostname
   chmod 600 /var/lib/tor/hidden_service/hs_ed25519_public_key
