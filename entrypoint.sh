13ad22c7a39379d28b512237b94e4a0beb858525
XaetaCore <49328943+xaetacorenet@users.noreply.github.com>
2025-11-07 15:30:21 +0000

    Fixed order

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 if [ -e "/data/hostname" ] && [ -e "/data/hs_ed25519_public_key" ] && [ -e "/data/hs_ed25519_secret_key" ]; then 
-  chown -R debian-tor:debian-tor /var/lib/tor/hidden_service/ && 
-  chmod 700 /var/lib/tor/hidden_service/ && 
-  chmod 600 /var/lib/tor/hidden_service/hostname && 
-  chmod 600 /var/lib/tor/hidden_service/hs_ed25519_public_key && 
-  chmod 600 /var/lib/tor/hidden_service/hs_ed25519_secret_key &&
+  chown -R debian-tor:debian-tor /var/lib/tor/hidden_service/ 
   cp -r /data/* /var/lib/tor/hidden_service/
+  chmod 700 /var/lib/tor/hidden_service/ 
+  chmod 600 /var/lib/tor/hidden_service/hostname 
+  chmod 600 /var/lib/tor/hidden_service/hs_ed25519_public_key 
+  chmod 600 /var/lib/tor/hidden_service/hs_ed25519_secret_key   
 else 
   echo "Required files are missing or /data directory does not exist. Exiting."; 
   exit 1; 
